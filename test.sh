8601e8bf95a55f93bec6cff6e4514b392e7af3d4
Simon Walker <s.r.walker101@googlemail.com>
2014-07-24 14:10:12 +0100

    Not removing initial directory for now

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ main() {
     local readonly rootdir=../zlp-run-script/testdata
     local readonly plotdir=/tmp/plots
 
-    test -d ${plotdir} && rm -rf ${plotdir}
+    # test -d ${plotdir} && rm -rf ${plotdir}
 
     bash ./run.sh ${rootdir} ${plotdir}
 }
