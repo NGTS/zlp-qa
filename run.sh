67ab850226823b6397550b9acd08c83df7f64e31
Simon Walker <s.r.walker101@googlemail.com>
2014-07-28 18:03:30 +0100

    Splits out extracted astrometric parameters

--- run.sh
+++ run.sh
@@ -105,6 +105,21 @@ plot_photometric_time_series() {
 
 }
 
+plot_extracted_astrometic_parameters() {
+    local readonly rootdir="$1"
+    local readonly plotsdir="$2"
+
+    OUTPUTFILE="${plotsdir}/09-extracted-astrometric-parameters.${EXT}"
+    if [[ ! -f ${OUTPUTFILE} ]]; then
+        find -L ${rootdir}/Reduction/output/ -name 'proc*.fits' | grep image > ${TMPDIR}/science-images-list.txt
+        python astrometry/extract_wcs_parameters.py ${TMPDIR}/science-images-list.txt -o ${TMPDIR}/astrometric-extraction.csv
+        python astrometry/plot_astrometric_parameters.py ${TMPDIR}/astrometric-extraction.csv -o ${OUTPUTFILE}
+    else
+        echo "Output file ${OUTPUTFILE} exists, skipping"
+    fi
+
+}
+
 make_images() {
     local readonly rootdir=$(abspath $1)
     local readonly outputdir=$(abspath $2)
@@ -118,15 +133,7 @@ make_images() {
     plot_flux_vs_rms "${rootdir}" "${plotsdir}"
     plot_rms_vs_time "${rootdir}" "${plotsdir}"
     plot_photometric_time_series "${rootdir}" "${plotsdir}"
-
-    OUTPUTFILE="${plotsdir}/09-extracted-astrometric-parameters.${EXT}"
-    if [[ ! -f ${OUTPUTFILE} ]]; then
-        find -L ${rootdir}/Reduction/output/ -name 'proc*.fits' | grep image > ${TMPDIR}/science-images-list.txt
-        python astrometry/extract_wcs_parameters.py ${TMPDIR}/science-images-list.txt -o ${TMPDIR}/astrometric-extraction.csv
-        python astrometry/plot_astrometric_parameters.py ${TMPDIR}/astrometric-extraction.csv -o ${OUTPUTFILE}
-    else
-        echo "Output file ${OUTPUTFILE} exists, skipping"
-    fi
+    plot_extracted_astrometic_parameters "${rootdir}" "${plotsdir}"
 
     OUTPUTFILE="${plotsdir}/10-pixel-centre-of-mass.${EXT}"
     if [[ ! -f ${OUTPUTFILE} ]]; then
