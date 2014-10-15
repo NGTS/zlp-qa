984ca181eca39948a20910b4213c3702ba74682c
Simon Walker <s.r.walker101@googlemail.com>
2014-10-15 16:21:59 +0100

    Add script to plot flat adu totals

---
+++ reduction/plot_total_flat_adu.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import numpy as np
+import fitsio
+from qa_common import plt
+
+
+def main(args):
+    if not args.width % 2 == 0:
+        raise RuntimeError("Width must be a multiple of 2")
+
+    with fitsio.FITS(args.filename) as infile:
+        image_data = infile[0].read()
+
+    region = image_data[
+        args.y - args.width / 2: args.y + args.width / 2,
+        args.x - args.width / 2: args.x + args.width / 2
+    ]
+
+    med_region = np.median(region)
+    std_region = np.std(region)
+
+    fig, axis = plt.subplots()
+    colour_cycle = axis._get_lines.color_cycle
+    axis.hist(region.flatten(), bins=args.nbins, histtype='step', normed=True)
+    axis.axvline(med_region, color=next(colour_cycle))
+
+    axis.set_xlabel(r'Total counts / ADU')
+    axis.set_ylabel(r'Probability density')
+    axis.set_title('Median: {:.1f}, std: {:.3f}'.format(
+        med_region, std_region))
+
+    fig.tight_layout()
+    fig.savefig(args.output)
+
+
+if __name__ == '__main__':
+    default_width = 256
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('-o', '--output', required=True)
+    parser.add_argument('--width',
+                        help='Region width [default: {}]'.format(default_width),
+                        default=default_width, required=False, type=int)
+    parser.add_argument('--x', help='Region centre x [default: 1024]',
+                        default=1024, required=False, type=int)
+    parser.add_argument('--y', help='Region centre y [default: 1024]',
+                        default=1024, required=False, type=int)
+    parser.add_argument('--nbins', help='Number of histogram bins [default: 64]',
+                        default=64, required=False, type=int)
+    main(parser.parse_args())
