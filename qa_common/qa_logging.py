8c3a44be57576a219c8b4c47528e10309e7b9b07
Simon Walker <s.r.walker101@googlemail.com>
2014-10-15 17:51:36 +0100

    Logging to stdout by default

--- qa_common/qa_logging.py
+++ qa_common/qa_logging.py
@@ -1,7 +1,8 @@
 import logging
+import sys
 
 fmt = '%(asctime)s|%(name)s|%(levelname)s: %(message)s'
-logging.basicConfig(level=logging.DEBUG, format=fmt)
+logging.basicConfig(level=logging.DEBUG, format=fmt, stream=sys.stdout)
 
 def get_logger(name):
     return logging.getLogger(name)
