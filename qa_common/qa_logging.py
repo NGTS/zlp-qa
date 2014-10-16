d7b71a441a5e63ccb7946e29e96635f00d711674
Simon Walker <s.r.walker101@googlemail.com>
2014-10-16 11:16:41 +0100

    Add structured logging framework

--- qa_common/qa_logging.py
+++ qa_common/qa_logging.py
@@ -1,8 +1,29 @@
 import logging
 import sys
+import structlog
+import datetime
 
-fmt = '%(asctime)s|%(name)s|%(levelname)s: %(message)s'
+fmt = '%(message)s'
 logging.basicConfig(level=logging.DEBUG, format=fmt, stream=sys.stdout)
 
-def get_logger(name):
-    return logging.getLogger(name)
+def add_timestamp(_, __, event_dict):
+    event_dict['timestamp'] = str(datetime.datetime.utcnow())
+    return event_dict
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.filter_by_level,
+        add_timestamp,
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.JSONRenderer()
+    ],
+    context_class=dict,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
+
+
+def get_logger(filename):
+    return structlog.get_logger().bind(source=filename)
