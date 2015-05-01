e151d076d8fcd68845fc80d3dc3e5659ddffd766
Simon Walker <s.r.walker101@googlemail.com>
2015-05-01 11:41:02 +0100

    Add fabfile to deploy help text

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from fabric.api import *
+
+env.use_ssh_config = True
+env.hosts = ['ngtshead.astro']
+
+@task
+def deploy():
+    build()
+    copy_to_destination()
+
+@task
+def build():
+    local('make -C help')
+
+
+@task
+def copy_to_destination():
+    with cd('/ngts/pipeline/qa'):
+        run('mkdir -p help')
+        put('help/build/*', 'help/')
