053c423e2098a6d16d48417755336eef9296923d
Simon Walker <s.r.walker101@googlemail.com>
2015-05-07 11:25:59 +0100

    Move compute_limits function
    ...to plotting part of qa_common

--- qa_common/plotting.py
+++ qa_common/plotting.py
@@ -6,3 +6,14 @@ matplotlib.rc('patch', edgecolor='None')
 matplotlib.rc('image', cmap='afmhot')
 matplotlib.rc('figure', figsize=(11, 8))
 
+
+def compute_limits(data, nsigma=3, precomputed_median=None):
+    med = (precomputed_median if precomputed_median is not None else
+           np.median(data))
+
+    std = np.std(data)
+
+    ll = med - nsigma * std
+    ul = med + nsigma * std
+
+    return ll, ul
