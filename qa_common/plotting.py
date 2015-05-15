ff0637e4c690049de51ba20ae8b111da281e4c7d
Simon Walker <s.r.walker101@googlemail.com>
2015-05-15 16:42:44 +0100

    Add nice subplots helper

--- qa_common/plotting.py
+++ qa_common/plotting.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from contextlib import contextmanager
 
 matplotlib.rc('patch', edgecolor='None')
 matplotlib.rc('image', cmap='afmhot')
@@ -17,3 +18,26 @@ def compute_limits(data, nsigma=3, precomputed_median=None):
     ul = med + nsigma * std
 
     return ll, ul
+
+
+@contextmanager
+def subplots(*args, **kwargs):
+    xlabels = kwargs.pop('xlabels', None)
+    ylabels = kwargs.pop('ylabels', None)
+    grid = kwargs.pop('grid', None)
+
+    fig, axes = plt.subplots(*args, **kwargs)
+    try:
+        axes = axes.flatten()
+    except AttributeError:
+        axes = [axes]
+    yield (fig, axes)
+    if xlabels is not None:
+        for (ax, label) in zip(axes, xlabels):
+            ax.set_xlabel(label)
+
+    if ylabels is not None:
+        for (ax, label) in zip(axes, ylabels):
+            ax.set_ylabel(label)
+
+    fig.tight_layout()
