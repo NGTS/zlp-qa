7f3e312e64c349be8609e84c71ca386cc2e14335
Simon Walker <s.r.walker101@googlemail.com>
2015-06-25 11:52:11 +0100

    Add help strings to fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,16 +5,25 @@ env.hosts = ['ngtshead.astro']
 
 @task(default=True)
 def deploy():
+    '''
+    Build the help pages and deploy to ngtshead
+    '''
     build()
     copy_to_destination()
 
 @task
 def build():
+    '''
+    Build the help pages
+    '''
     local('make -C help')
 
 
 @task
 def copy_to_destination():
+    '''
+    Deploy help pages to ngtshead
+    '''
     with cd('/ngts/pipeline/qa'):
         run('mkdir -p help')
         put('help/build/*', 'help/')
